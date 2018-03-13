94ccf01ca4e11a8f856d3825775020fa71d295b2
parklover <slawekpianino96@gmail.com>
2018-03-13 09:12:28 +0100

    o ja
    asdhbsdfhkf

---
+++ PTMHelloGitLewandowski.cpp
@@ -0,0 +1,16 @@
+// PTMHelloGitLewandowski.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+
+	cout << "Hello Git!"<<endl;
+	cout << "ZMIANA KURWO" << endl;
+    return 0;
+}
+
